0e47ebb8681244380e8c36cad6e1fd47eee5b509
Matias Urra <matiexe31@gmail.com>
2019-11-14 21:42:07 -0300

    fin seccion 5- firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,14 +8,28 @@ export class InfoPaginaService {
 
   info:any = {};
   cargada = false;
+  equipo : any[]= [];
   constructor(private http : HttpClient) { 
     console.log('servicio listo');
-
+    this.cargarInfo();
+    this.cargarEquipo();
     //leer archivo Json
+    
+  }
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
     .subscribe(resp =>{
       this.cargada= true;
       this.info = resp;
     })
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-template-b1d18.firebaseio.com/equipo.json')
+    .subscribe((resp:any)=>{
+      this.equipo = resp;
+      console.log(resp);
+    })
+    
   }
 }
